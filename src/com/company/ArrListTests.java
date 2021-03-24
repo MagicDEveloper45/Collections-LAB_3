e1bbf5b75ff7b69987a11426b982d01fac34a9e2
MagicDEveloper45 <56794850+MagicDEveloper45@users.noreply.github.com>
2021-03-24 22:16:49 +0300

    Commit 2
    Created 2 class for collections + changed main()

---
+++ src/com/company/ArrListTests.java
@@ -0,0 +1,160 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class ArrListTests {
+
+     ArrayList<Integer> arrayList = new ArrayList<>();
+
+     private final int arrListLenght = 1000;
+
+     private long globalStart, globalFinish, globalElapsed;
+     public long add, get, clone, remove;
+
+     public void ShowResult()
+     {
+         arrList_ADD();
+         arrList_GET();
+         arrList_CLONE();
+         arrList_REMOVE();
+     }
+
+     public void arrList_ADD()
+     {
+         System.out.println("\n 1. ArrayList add - tests");
+         globalStart = System.nanoTime();
+
+         int leftBorder = 0;
+         int rightBorder = 5000;
+
+         for (int i = 0; i < arrListLenght;i++)
+         {
+             int value =  leftBorder + (int) (Math.random() * rightBorder); // taking random number from 0 to 1000
+
+             long start = System.nanoTime();
+
+             arrayList.add(i, value);
+
+             long finish = System.nanoTime();
+             long elapsed = finish - start;
+
+             System.out.println(i + "th " + "add " + elapsed  + " ns");
+         }
+
+         globalFinish = System.nanoTime();
+         globalElapsed = globalFinish - globalStart;
+
+         System.out.println("\nTotal time = " + globalElapsed  + " ns, or ~ " + globalElapsed / 1000000 + " ms");
+
+         add = globalElapsed / 1000000;
+
+     }
+
+     public void arrListGet(boolean way)
+     {
+         if (way == true) {
+             randomInit();
+         }
+         arrList_GET();
+     }
+
+     private void arrList_GET()
+    {
+        System.out.println("\n 2. ArrayList get - tests");
+        globalStart = System.nanoTime();
+
+        // Test arraylist.get
+        for (int i = 0; i < arrListLenght;i++)
+        {
+            long start = System.nanoTime();
+
+            int number = arrayList.get(i);
+
+            long finish = System.nanoTime();
+            long elapsed = finish - start;
+
+            System.out.println(i + "th " + "add " + elapsed  + " ns. Number = " + number);
+        }
+
+        long globalFinish = System.nanoTime();
+        long globalElapsed = globalFinish - globalStart;
+
+        System.out.println("\nTotal time = " + globalElapsed  + " ns, or ~ " + globalElapsed / 1000000 + " ms");
+
+        get = globalElapsed / 1000000;
+
+    }
+
+    public void arrListClone(boolean way)
+    {
+        if (way == true) {
+            randomInit();
+        }
+        arrList_CLONE();
+    }
+
+    private void arrList_CLONE()
+    {
+        System.out.println("\n 3. ArrayList clone - tests");
+        globalStart = System.nanoTime();
+
+        ArrayList arrayListClone = new ArrayList<>(arrayList.size());
+
+        // Test arraylist.get
+        arrayListClone = (ArrayList)arrayList.clone();
+
+        globalFinish = System.nanoTime();
+        globalElapsed = globalFinish - globalStart;
+
+        System.out.println("\nTotal time = " + globalElapsed  + " ns, or ~ " + globalElapsed / 1000000 + " ms");
+
+        clone = globalElapsed;
+
+    }
+
+    public void arrListRemove(boolean way)
+    {
+        if (way == true) {
+            randomInit();
+        }
+        arrList_REMOVE();
+    }
+
+    private void arrList_REMOVE() {
+        System.out.println("\n 4. ArrayList remove - tests");
+        globalStart = System.nanoTime();
+
+        // Test arraylist.remove
+        for (int i = 0; i < arrListLenght / 2 - 1; i++) {
+            long start = System.nanoTime();
+
+            arrayList.remove(i);
+
+            long finish = System.nanoTime();
+            long elapsed = finish - start;
+
+            System.out.println(i + "th " + "add " + elapsed + " ns");
+        }
+
+        globalFinish = System.nanoTime();
+        globalElapsed = globalFinish - globalStart;
+
+        System.out.println("\nTotal time = " + globalElapsed + " ns, or ~ " + globalElapsed / 1000000 + " 1ms");
+
+        remove = globalElapsed / 1000000;
+
+    }
+
+    private void randomInit()
+    {
+        int leftBorder = 0;
+        int rightBorder = 5000;
+
+        for (int i = 0; i < arrListLenght;i++)
+        {
+            int value =  leftBorder + (int) (Math.random() * rightBorder); // taking random number from 0 to 1000
+            arrayList.add(i, value);
+        }
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,197 +6,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void ArrListTests()
-    {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        int leftBorder = 0, rightBorder = 5000;
-        int arrayListLenght = 1000;
-
-        /***************************************************************************************************************/
-
-        System.out.println("\n 1. ArrayList add - tests");
-        long globalStart_1 = System.nanoTime();
-
-        // Test arraylist.add
-        for (int i = 0; i < arrayListLenght;i++)
-        {
-            int value =  leftBorder + (int) (Math.random() * rightBorder); // taking random number from 0 to 1000
-
-            long start = System.nanoTime();
-
-            arrayList.add(i, value);
-
-            long finish = System.nanoTime();
-            long elapsed = finish - start;
-
-            System.out.println(i + "th " + "add " + elapsed  + " ns");
-        }
-
-        long globalFinish_1 = System.nanoTime();
-        long globalElapsed_1 = globalFinish_1 - globalStart_1;
-
-        System.out.println("\nTotal time = " + globalElapsed_1  + " ns, or ~ " + globalElapsed_1 / 1000000 + " ms");
-
-        /***************************************************************************************************************/
-
-        System.out.println("\n 2. ArrayList get - tests");
-        long globalStart_2 = System.nanoTime();
-
-        // Test arraylist.get
-        for (int i = 0; i < arrayListLenght;i++)
-        {
-            long start = System.nanoTime();
-
-            int number = arrayList.get(i);
-
-            long finish = System.nanoTime();
-            long elapsed = finish - start;
-
-            System.out.println(i + "th " + "add " + elapsed  + " ns. Number = " + number);
-        }
-
-        long globalFinish_2 = System.nanoTime();
-        long globalElapsed_2 = globalFinish_2 - globalStart_2;
-
-        System.out.println("\nTotal time = " + globalElapsed_2  + " ns, or ~ " + globalElapsed_2 / 1000000 + " ms");
-
-        /***************************************************************************************************************/
-
-        System.out.println("\n 3. ArrayList clone - tests");
-        long globalStart_3 = System.nanoTime();
-
-        ArrayList<Integer> arrayListClone = new ArrayList<>(arrayList.size());
-
-        // Test arraylist.get
-        arrayListClone = (ArrayList)arrayList.clone();
-
-        long globalFinish_3 = System.nanoTime();
-        long globalElapsed_3 = globalFinish_3 - globalStart_3;
-
-        System.out.println("\nTotal time = " + globalElapsed_3  + " ns, or ~ " + globalElapsed_3 / 1000000 + " ms");
-
-        /***************************************************************************************************************/
-
-        System.out.println("\n 4. ArrayList remove - tests");
-        long globalStart_4 = System.nanoTime();
-
-        // Test arraylist.remove
-        for (int i = 0; i < arrayListLenght/2 - 1;i++)
-        {
-            long start = System.nanoTime();
-
-            arrayList.remove(i);
-
-            long finish = System.nanoTime();
-            long elapsed = finish - start;
-
-            System.out.println(i + "th " + "add " + elapsed  + " ns");
-        }
-
-        long globalFinish_4 = System.nanoTime();
-        long globalElapsed_4 = globalFinish_4 - globalStart_4;
-
-        System.out.println("\nTotal time = " + globalElapsed_4  + " ns, or ~ " + globalElapsed_4 / 1000000 + " 1ms");
-    }
-
-    public static void LinkListTests()
-    {
-        LinkedList<Integer> arrayList = new LinkedList<>();
-
-        int leftBorder = 0, rightBorder = 5000;
-        int arrayListLenght = 1000;
-
-        /***************************************************************************************************************/
-
-        System.out.println("\n 1. ArrayList add - tests");
-        long globalStart_1 = System.nanoTime();
-
-        // Test arraylist.add
-        for (int i = 0; i < arrayListLenght;i++)
-        {
-            int value =  leftBorder + (int) (Math.random() * rightBorder); // taking random number from 0 to 1000
-
-            long start = System.nanoTime();
-
-            arrayList.add(i, value);
-
-            long finish = System.nanoTime();
-            long elapsed = finish - start;
-
-            System.out.println(i + "th " + "add " + elapsed  + " ns");
-        }
-
-        long globalFinish_1 = System.nanoTime();
-        long globalElapsed_1 = globalFinish_1 - globalStart_1;
-
-        System.out.println("\nTotal time = " + globalElapsed_1  + " ns, or ~ " + globalElapsed_1 / 1000000 + " ms");
-
-        /***************************************************************************************************************/
-
-        System.out.println("\n 2. ArrayList get - tests");
-        long globalStart_2 = System.nanoTime();
-
-        // Test arraylist.get
-        for (int i = 0; i < arrayListLenght;i++)
-        {
-            long start = System.nanoTime();
-
-            int number = arrayList.get(i);
-
-            long finish = System.nanoTime();
-            long elapsed = finish - start;
-
-            System.out.println(i + "th " + "add " + elapsed  + " ns. Number = " + number);
-        }
-
-        long globalFinish_2 = System.nanoTime();
-        long globalElapsed_2 = globalFinish_2 - globalStart_2;
-
-        System.out.println("\nTotal time = " + globalElapsed_2  + " ns, or ~ " + globalElapsed_2 / 1000000 + " ms");
-
-        /***************************************************************************************************************/
-
-        System.out.println("\n 3. ArrayList clone - tests");
-        long globalStart_3 = System.nanoTime();
-
-        LinkedList<Integer> arrayListClone = new LinkedList<Integer>();
-
-        // Test arraylist.get
-        arrayListClone = (LinkedList)arrayList.clone();
-
-        long globalFinish_3 = System.nanoTime();
-        long globalElapsed_3 = globalFinish_3 - globalStart_3;
-
-        System.out.println("\nTotal time = " + globalElapsed_3  + " ns, or ~ " + globalElapsed_3 / 1000000 + " ms");
-
-        /***************************************************************************************************************/
-
-        System.out.println("\n 4. ArrayList remove - tests");
-        long globalStart_4 = System.nanoTime();
-
-        // Test arraylist.remove
-        for (int i = 0; i < arrayListLenght/2 - 1;i++)
-        {
-            long start = System.nanoTime();
-
-            arrayList.remove(i);
-
-            long finish = System.nanoTime();
-            long elapsed = finish - start;
-
-            System.out.println(i + "th " + "add " + elapsed  + " ns");
-        }
-
-        long globalFinish_4 = System.nanoTime();
-        long globalElapsed_4 = globalFinish_4 - globalStart_4;
-
-        System.out.println("\nTotal time = " + globalElapsed_4  + " ns, or ~ " + globalElapsed_4 / 1000000 + " 1ms");
-    }
-
     public static void ShowResult()
     {
+        LinkedListTests linkedListTests = new LinkedListTests();
+        ArrListTests arrListTests = new ArrListTests();
 
+        linkedListTests.ShowResult();
+        arrListTests.ShowResult();
+
+        System.out.println("Result");
+        System.out.println("* Add: \n ArrayList: " + arrListTests.add + " ms, LinkedList " + linkedListTests.add + " ms" );
+        System.out.println("* Get: \n ArrayList: " + arrListTests.get + " ms, LinkedList " + linkedListTests.get + " ms");
+        System.out.println("* Clone: \n ArrayList: " + arrListTests.clone + " ns, LinkedList " + linkedListTests.clone + " ns" );
+        System.out.println("* Remove: \n ArrayList: " + arrListTests.remove + " ms, LinkedList " + linkedListTests.remove + " ms");
     }
     public static void main(String[] args) {
 
@@ -206,13 +28,48 @@ public class Main {
         do {
             System.out.println("Choose type of collection \nArrayList - 1, LinkedList - 2, Show result - 3 \ninput:");
             choose = Integer.parseInt(in.nextLine());
-        }while (choose != 1 && choose != 2);
+        }while (choose <=0 && choose >=4);
 
         switch (choose)
         {
-            case 1:ArrListTests();
-            case 2:LinkListTests();
-            case 3: ShowResult();
+            case 1:
+            {
+                ArrListTests problem1 = new ArrListTests();
+                do {
+                    System.out.println("Choose method: add - 1, get - 2, clone - 3, remove - 4");
+                    choose = Integer.parseInt(in.nextLine());
+                }while (choose <= 0 && choose >= 5);
+
+                switch (choose)
+                {
+                    case 1: problem1.arrList_ADD(); break;
+                    case 2: problem1.arrListGet(true); break;
+                    case 3: problem1.arrListClone(true); break;
+                    case 4: problem1.arrListRemove(true); break;
+                    case 5: problem1.ShowResult(); break;
+                    default: return;
+                }
+
+            }
+             case 2:
+             {
+                 LinkedListTests problem2 = new LinkedListTests();
+                 do {
+                     System.out.println("Choose method: add - 1, get - 2, clone - 3, remove - 4");
+                     choose = Integer.parseInt(in.nextLine());
+                 }while (choose <= 0 && choose >= 5);
+
+                 switch (choose)
+                 {
+                     case 1: problem2.arrList_ADD(); break;
+                     case 2: problem2.arrListGet(true); break;
+                     case 3: problem2.arrListClone(true); break;
+                     case 4: problem2.arrListRemove(true); break;
+                     case 5: problem2.ShowResult(); break;
+                     default: return;
+                 }
+             }
+             case 3: ShowResult();
             default: return;
         }
 
